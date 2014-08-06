2089260d2cb33bf0451a07865a1eb6c52f9522c4
benjaminbertin <benjamin.bertin@gmail.com>
2014-08-06 16:14:23 +0200

    Added a method to add a Value to another Value with proper uncertainty calculation

--- src/main/java/com/mycsense/carbondb/Value.java
+++ src/main/java/com/mycsense/carbondb/Value.java
@@ -8,4 +8,15 @@ public class Value {
         this.value = value;
         this.uncertainty = uncertainty;
     }
+
+    public Value add(Value v2) {
+        value += v2.value;
+        if (Math.abs(value) == 0)
+            uncertainty = 0.0;
+        else
+            uncertainty = Math.sqrt(Math.pow(value * uncertainty, 2)
+                                    + Math.pow(v2.value * v2.uncertainty, 2))
+                          / Math.abs(value);
+        return this;
+    }
 }
