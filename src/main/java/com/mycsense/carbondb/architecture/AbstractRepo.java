a956ae713ebab08108eeeef68ba13126fd15e475
benjaminbertin <benjamin.bertin@gmail.com>
2014-11-25 11:26:20 +0100

    Changed the logger name for the abstract repo

--- src/main/java/com/mycsense/carbondb/architecture/AbstractRepo.java
+++ src/main/java/com/mycsense/carbondb/architecture/AbstractRepo.java
@@ -11,7 +11,7 @@ import org.slf4j.LoggerFactory;
 public abstract class AbstractRepo {
     public Model model;
 
-    protected final Logger log = LoggerFactory.getLogger(UnitToolsWebService.class);
+    protected final Logger log = LoggerFactory.getLogger(AbstractRepo.class);
 
     public AbstractRepo (Model model) {
         this.model = model;
